8283ed6ac3c9ccfc4897cb11d21b3ba91e2df196
clice <clice.bbr@gmail.com>
2023-09-25 20:06:59 -0300

    Organizando Arquivos

--- Arquivos/heap.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "Filadefoguetes.h"
-#include "heap.h"
-#define F_esq(i) (2*i+1)
-#define F_dir(i) (2*i+2)
-#define PAI(i)
-#define swap(a,b) {Nave tmp=*a; *a=*b ;*b=tmp;}
-
--- Arquivos/heap.h
+++
@@ -1,8 +0,0 @@
-#define HEAP_H_INCLUDED
-#define F_esq(i) (2*i+1)
-#define F_dir(i) (2*i+2)
-#define PAI(i)
-#define swap(a,b) {Nave tmp=*a; *a=*b ;*b=tmp;}
-
-
-
